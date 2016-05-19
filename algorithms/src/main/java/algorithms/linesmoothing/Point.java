0b0945bf5bb159e8c3e34b9880897d71a602aeb1
keyur.mahajan <keyur.mahajan1988@gmail.com>
2016-05-19 18:55:06 +0530

    Point class coordinates converted to double

--- algorithms/src/main/java/algorithms/linesmoothing/Point.java
+++ algorithms/src/main/java/algorithms/linesmoothing/Point.java
@@ -7,10 +7,10 @@ package algorithms.linesmoothing;
  *
  */
 public class Point {
-	private int x;
-	private int y;
+	private double x;
+	private double y;
 
-	public Point(int x, int y) {
+	public Point(double x, double y) {
 		this.setX(x);
 		this.setY(y);
 
@@ -21,7 +21,7 @@ public class Point {
 	 *
 	 * @return X
 	 */
-	public int getX() {
+	public double getX() {
 		return x;
 	}
 
@@ -30,7 +30,7 @@ public class Point {
 	 *
 	 * @param x
 	 */
-	public void setX(int x) {
+	public void setX(double x) {
 		this.x = x;
 	}
 
@@ -39,7 +39,7 @@ public class Point {
 	 *
 	 * @return y
 	 */
-	public int getY() {
+	public double getY() {
 		return y;
 	}
 
@@ -48,7 +48,7 @@ public class Point {
 	 *
 	 * @param y
 	 */
-	public void setY(int y) {
+	public void setY(double y) {
 		this.y = y;
 	}
 }
